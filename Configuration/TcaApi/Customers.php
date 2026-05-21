6ef72c33a91856cca5ff66ebeb12f8039bd23be3
Maik Schneider <schneider.maik@me.com>
2026-05-21 15:19:09 +0200

    [TASK] Require maikschneider/tca-api and add API resource configuration

---
+++ Configuration/TcaApi/Customers.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'general' => [
+        'table' => 'tx_typo3petstore_domain_model_customer',
+        'resourceName' => 'customers',
+        'resourceType' => 'Customer',
+    ],
+];
--- ext_emconf.php
+++ ext_emconf.php
@@ -13,6 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.0-14.99.99',
+            'tca_api' => '',
         ],
         'conflicts' => [],
         'suggests' => [],
