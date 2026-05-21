92524b00e5f4eed6143791840e110809caa89320
Maik Schneider <schneider.maik@me.com>
2026-05-21 10:16:18 +0200

    [TASK] Add strict_types declaration to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TYPO3 Petstore',
     'description' => 'Demo extension showcasing all TCA field types and relation patterns via the classic Swagger Petstore domain model.',
