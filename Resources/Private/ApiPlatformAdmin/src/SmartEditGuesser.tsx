a8d562ebbee707dd83d16a71d5191c6c314f8568
Maik Schneider <schneider.maik@me.com>
2026-05-27 10:07:25 +0200

    [BUGFIX] Reformat with editorconfig

--- Resources/Private/ApiPlatformAdmin/src/SmartEditGuesser.tsx
+++ Resources/Private/ApiPlatformAdmin/src/SmartEditGuesser.tsx
@@ -1,29 +1,26 @@
-import { EditGuesser, Introspecter } from "@api-platform/admin";
-import type { Field } from "@api-platform/api-doc-parser";
-import { useResourceContext } from "react-admin";
-import { SmartInputGuesser } from "./SmartInputGuesser";
+import {EditGuesser, Introspecter} from "@api-platform/admin";
+import type {Field} from "@api-platform/api-doc-parser";
+import {useResourceContext} from "react-admin";
+import {SmartInputGuesser} from "./SmartInputGuesser";
 
 interface IntrospectedProps {
-  writableFields: Field[];
-  [key: string]: unknown;
+    writableFields: Field[];
+
+    [key: string]: unknown;
 }
 
-const IntrospectedSmartEdit = ({ writableFields, ...props }: IntrospectedProps) => (
-  <EditGuesser {...props}>
-    {writableFields.map((field) => (
-      <SmartInputGuesser key={field.name} source={field.name} />
-    ))}
-  </EditGuesser>
+const IntrospectedSmartEdit = ({writableFields, ...props}: IntrospectedProps) => (
+    <EditGuesser {...props}>
+        {writableFields.map((field) => (
+            <SmartInputGuesser key={field.name} source={field.name} />
+        ))}
+    </EditGuesser>
 );
 
 export const SmartEditGuesser = (props: Record<string, unknown>) => {
-  const resource = useResourceContext(props as { resource?: string });
-  return (
-    <Introspecter
-      component={IntrospectedSmartEdit}
-      resource={resource!}
-      includeDeprecated={true}
-      {...props}
-    />
-  );
+    const resource = useResourceContext(props as { resource?: string });
+    return (
+        <Introspecter
+            component={IntrospectedSmartEdit} resource={resource!} includeDeprecated={true}{...props} />
+    );
 };
--- Resources/Private/ApiPlatformAdmin/src/SmartShowGuesser.tsx
+++ Resources/Private/ApiPlatformAdmin/src/SmartShowGuesser.tsx
@@ -1,29 +1,26 @@
-import { Introspecter, ShowGuesser } from "@api-platform/admin";
-import type { Field } from "@api-platform/api-doc-parser";
-import { useResourceContext } from "react-admin";
-import { SmartFieldGuesser } from "./SmartFieldGuesser";
+import {Introspecter, ShowGuesser} from "@api-platform/admin";
+import type {Field} from "@api-platform/api-doc-parser";
+import {useResourceContext} from "react-admin";
+import {SmartFieldGuesser} from "./SmartFieldGuesser";
 
 interface IntrospectedProps {
-  readableFields: Field[];
-  [key: string]: unknown;
+    readableFields: Field[];
+
+    [key: string]: unknown;
 }
 
-const IntrospectedSmartShow = ({ readableFields, ...props }: IntrospectedProps) => (
-  <ShowGuesser {...props}>
-    {readableFields.map((field) => (
-      <SmartFieldGuesser key={field.name} source={field.name} />
-    ))}
-  </ShowGuesser>
+const IntrospectedSmartShow = ({readableFields, ...props}: IntrospectedProps) => (
+    <ShowGuesser {...props}>
+        {readableFields.map((field) => (
+            <SmartFieldGuesser key={field.name} source={field.name} />
+        ))}
+    </ShowGuesser>
 );
 
 export const SmartShowGuesser = (props: Record<string, unknown>) => {
-  const resource = useResourceContext(props as { resource?: string });
-  return (
-    <Introspecter
-      component={IntrospectedSmartShow}
-      resource={resource!}
-      includeDeprecated={true}
-      {...props}
-    />
-  );
+    const resource = useResourceContext(props as { resource?: string });
+    return (
+        <Introspecter
+            component={IntrospectedSmartShow} resource={resource!} includeDeprecated={true}{...props} />
+    );
 };
